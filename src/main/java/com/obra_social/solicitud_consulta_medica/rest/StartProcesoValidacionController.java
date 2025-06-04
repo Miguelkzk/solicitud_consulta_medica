5b237f026d467901b4df53d3218e1cbad7f5d61b
Miguel Kruzliak <141929749+Miguelkzk@users.noreply.github.com>
2025-06-04 00:00:06 -0300

    basic config

--- src/main/java/com/obra_social/solicitud_consulta_medica/rest/StartProcesoValidacionController.java
+++ src/main/java/com/obra_social/solicitud_consulta_medica/rest/StartProcesoValidacionController.java
@@ -1,5 +1,35 @@
 package com.obra_social.solicitud_consulta_medica.rest;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.camunda.client.CamundaClient;
+
+@RestController
+@RequestMapping("/")
 public class StartProcesoValidacionController {
-    
+  private static final Logger LOG = LoggerFactory.getLogger(StartProcesoValidacionController.class);
+   private static final String PROCESS_ID = "Process_0x09901"; // ID del proceso BPMN
+
+  @Autowired private CamundaClient zeebe;
+
+  @PostMapping("/start")
+  public void startProcessInstance(@RequestBody Map<String, Object> variables) {
+
+    LOG.info("Iniciando proceso de validacion de paciente " + variables);
+
+    zeebe
+        .newCreateInstanceCommand()
+        .bpmnProcessId(PROCESS_ID)
+        .latestVersion()
+        .variables(variables)
+        .send();
+  }
 }
